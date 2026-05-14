0f935e93215bc47c28376a7283eb8890f61eae31
julesy19 <37917963+julesy19@users.noreply.github.com>
2026-05-14 00:18:03 -0400

    Add Jenkins page

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route('/jenkins')
+def jenkins():
+    return render_template('jenkins.html')
+
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
